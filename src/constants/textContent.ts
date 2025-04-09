5e06eec9b80cd55036a7ff1d195b138dbdd6e878
karthiknarayanv <karthikgogul6@gmail.com>
2025-04-09 18:58:42 +0530

    Logo and App Name added

---
+++ src/constants/textContent.ts
@@ -0,0 +1,7 @@
+const TEXT = {
+  appName: "UI Developer | Karthik Narayan",
+  metaDescription:
+    "Portfolio of Karthik Narayan – a passionate UI Developer crafting modern, responsive interfaces using React and React Native.",
+};
+
+export default TEXT;
